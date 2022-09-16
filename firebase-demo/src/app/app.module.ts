ae7912d1b0584de612e3e75a890843b22d21b5dd
huykaiser <huy@rawskill.gg>
2022-09-16 22:27:28 +0200

    upload

---
+++ firebase-demo/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { RegisterComponent } from './component/register/register.component';
+import { FormsModule } from '@angular/forms';
+import { ForgotPasswordComponent } from './component/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './component/verify-email/verify-email.component';
+import { FileuploadComponent } from './component/fileupload/fileupload.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    RegisterComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent,
+    FileuploadComponent
+  ],
+  
+  imports: [
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    FormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ firebase-demo/src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyBhMTswSCAx6sAcXuQgOk2bGa2pLEyhYNE",
+    authDomain: "fir-demo-b423e.firebaseapp.com",
+    databaseURL: "https://fir-demo-b423e-default-rtdb.firebaseio.com",
+    projectId: "fir-demo-b423e",
+    storageBucket: "fir-demo-b423e.appspot.com",
+    messagingSenderId: "318356995138",
+    appId: "1:318356995138:web:1f8e4b78712ac5882ac1db",
+    measurementId: "G-413YHYT0K9"
+  }
+};
